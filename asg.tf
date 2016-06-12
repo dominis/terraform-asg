ec8640761a125d60be6efa0f1d975f87f7aca0f6
Nandor Sivok <dominis@haxor.hu>
2016-06-13 01:19:05 +0200

    meh

--- asg.tf
+++ asg.tf
@@ -21,8 +21,8 @@ resource "aws_cloudformation_stack" "autoscaling_group" {
     "Asg": {
       "Type": "AWS::AutoScaling::AutoScalingGroup",
       "Properties": {
-        "AvailabilityZones": ["${split(",", var.availability_zones)}"],
-        "VPCZoneIdentifier": ["${split(",", var.private_subnets)}"],
+        "AvailabilityZones": ["${join(", ", formatlist("\\"%s\\"", split(",", var.availability_zones)))}"],
+        "VPCZoneIdentifier": ["${join(", ", formatlist("\\"%s\\"", split(",", var.private_subnets)))}"],
         "LaunchConfigurationName": "${aws_launch_configuration.launch_config.name}",
         "MaxSize": "${var.maximum_number_of_instances}",
         "MinSize": "${var.minimum_number_of_instances}",
